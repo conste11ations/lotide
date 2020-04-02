9a9f25b988e58d6f29fa6e860482c4f8840edb64
Rachel Kat <rachelkat@gmail.com>
2020-04-02 23:11:54 +0000

    add assertArraysEqual tests to takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -6,6 +6,28 @@ It will take in two parameters:
 2. The callback (which Lodash calls "predicate")
 */
 
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayA.length; i++) {
+      if (arrayA[i] !== arrayB[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log("💚🥬📗 Assertion Passed: ", actual, " === ", expected);
+  } else {
+    console.log("🍎🧧🔴 Assertion Failed: ", actual, " !== ", expected);
+  }
+};
+
 const takeUntil = function(array, callback) {
   let result = [];
   for (let item of array) {
@@ -20,10 +42,12 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1); // [ 1, 2, 5, 7, 2 ]
+const test1 = [ 1, 2, 5, 7, 2 ];
+assertArraysEqual(results1, test1); // [ 1, 2, 5, 7, 2 ]
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2); // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+const test2 = [ 'I\'ve', 'been', 'to', 'Hollywood' ];
+assertArraysEqual(results2, test2); // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
